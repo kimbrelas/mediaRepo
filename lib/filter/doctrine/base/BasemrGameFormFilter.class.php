17a46f8878d485407665484e32bd2841ede06c9e
Adam Kimbrel <kimbrelas@bradshaw-kimbrel.com>
2010-03-27 01:22:12 -0500

    phpedit project file / mrGame basic setup

---
+++ lib/filter/doctrine/base/BasemrGameFormFilter.class.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * mrGame filter form base class.
+ *
+ * @package    mediaRepo
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterGeneratedTemplate.php 24171 2009-11-19 16:37:50Z Kris.Wallsmith $
+ */
+abstract class BasemrGameFormFilter extends BaseFormFilterDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'name' => new sfWidgetFormFilterInput(),
+    ));
+
+    $this->setValidators(array(
+      'name' => new sfValidatorPass(array('required' => false)),
+    ));
+
+    $this->widgetSchema->setNameFormat('mr_game_filters[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'mrGame';
+  }
+
+  public function getFields()
+  {
+    return array(
+      'id'   => 'Number',
+      'name' => 'Text',
+    );
+  }
+}
