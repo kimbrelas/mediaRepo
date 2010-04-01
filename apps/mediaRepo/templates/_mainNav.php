0278aa3b37ff6ffba9ec6a4a183f308ceb6c883a
Adam Kimbrel <kimbrelas@bradshaw-kimbrel.com>
2010-04-01 00:20:45 -0500

    config files and main nav

--- apps/mediaRepo/templates/_mainNav.php
+++ apps/mediaRepo/templates/_mainNav.php
@@ -5,13 +5,3 @@
 	<li class="movies" <?php if($module == 'mrMovie'): ?>id="activated"<?php endif; ?>><a href="<?php echo url_for('movies') ?>">Movies</a></li>
 	<li class="music" <?php if($module == 'mrMusic'): ?>id="activated"<?php endif; ?>><a href="<?php echo url_for('music') ?>">Music</a></li>
 </ul>
-
-<?php /*
-<?php if(!$sf_user->isAuthenticated()): ?>
-	Not logged in
-	<a href="<?php echo url_for('sf_guard_signin') ?>">Login</a> or <a href="<?php echo url_for('register') ?>">Register</a>
-<?php else: ?>
-	Logged in as <?php echo $sf_user->getGuardUser()->username ?>
-	<a href="<?php echo url_for('sf_guard_signout') ?>">Logout</a>
-<?php endif; ?>
-*/ ?>
