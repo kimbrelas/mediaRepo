c1674b7c6313c2c3fe842edaa9b4b1fc33c2e10c
Adam Kimbrel <kimbrelas@kimbrelas-ubuntu.(none)>
2010-05-15 21:12:07 -0500

    fixed song delete link

--- apps/mediaRepo/modules/mrSong/actions/actions.class.php
+++ apps/mediaRepo/modules/mrSong/actions/actions.class.php
@@ -8,17 +8,20 @@
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class mrSongActions extends mrActions
+class mrSongActions extends sfActions
 {
   public function executeDelete(sfWebRequest $request)
   {
     $request->checkCSRFProtection();
-		
+    
+    $this->status = $request->getParameter('status');
     $song = $this->getRoute()->getObject();
     $album = $song->Album;
     
     $song->delete();
+    
+    $route = ($this->status == 'owned') ? 'music_show' : 'mr_music_'.$this->status.'_show';
 
-    $this->redirect($this->generateUrl($this->base_route.'_show', $album));
+    $this->redirect($this->generateUrl($route, $album));
   }
 }
